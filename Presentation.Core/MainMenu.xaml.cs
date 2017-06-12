e49ea76860d5d4f24ae8af18d8f5f040297a5faa
Peter Durrant <peterjdurrant@gmail.com>
2017-06-12 21:39:20 +0100

    Removed Presentation.Module - moved code to Presentation.Core

---
+++ Presentation.Core/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Hdd.Presentation.Core
+{
+    /// <summary>
+    ///     Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
